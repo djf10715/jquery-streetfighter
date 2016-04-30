fa30d9f10e863d7c304196b9c3b180d5f98f5482
David Felderman <feldermandj@Davids-MacBook-Pro.local>
2016-04-30 12:07:45 -0400

    streetfighter save through audio setup

--- js/app.js
+++ js/app.js
@@ -11,10 +11,19 @@ $('.ryu').mouseenter(function() {
     .mousedown(function() {
         console.log('mousedown');
         // play hadouken sound
+        playHadouken();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        $('.hadouken').show();
         // show hadouken and animate it to the right of the screen
+        $('.hadouken').finish().show()
+            .animate(
+            {'left' : '1020px'},
+        500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            }
+        );;
     })
     .mouseup(function() {
         console.log('mouseup');
@@ -24,3 +33,9 @@ $('.ryu').mouseenter(function() {
     });
 
 });
+
+function playHadouken () {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
